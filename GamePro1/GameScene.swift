86f737663263b498d43231cf8088786dc553434a
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-08-11 12:04:05 +0330

    Falling boxes: SKSpriteNode, UITouch, SKPhysicsBody

--- GamePro1/GameScene.swift
+++ GamePro1/GameScene.swift
@@ -9,6 +9,22 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background )
         
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        addChild(box)
     }
 }
